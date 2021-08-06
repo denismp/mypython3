55510fb3f48b21efd812fe8b0112a73231b6966b
Denis Putnam <denisputnam@verizon.net>
2021-08-06 16:42:25 -0400

    added set_root_log_level()

--- pylib/Utils/log_utils.py
+++ pylib/Utils/log_utils.py
@@ -5,9 +5,18 @@ class LogUtils():
     def __init__(self):
         self.VALID_LOG_LEVELS = ("DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL", "FATAL")
 
-    def set_logger(self, logger_name, log_level="INFO"):
-        logger = logging.getLogger(logger_name)
+    def set_root_log_level(self,log_level="INFO"):
         if log_level not in self.VALID_LOG_LEVELS:
             raise ValueError(f"Invalid Log Level.  It must be one of {self.VALID_LOG_LEVELS}")
-        logger.setLevel(logging.getLevelName(log_level))
-        return logger
+        logging.getLogger().setLevel(logging.getLevelName(log_level))
+
+
+    def set_logger(self, logger_name, log_level="INFO"):
+        #logger = logging.getLogger(logger_name)
+        if log_level not in self.VALID_LOG_LEVELS:
+            raise ValueError(f"Invalid Log Level.  It must be one of {self.VALID_LOG_LEVELS}")
+        #logger.setLevel(logging.getLevelName(log_level))
+        #return logger
+        logging.getLogger(logger_name).setLevel(logging.getLevelName(log_level))
+        logging.basicConfig(format='%(levelname)s:%(message)s')
+        return logging
