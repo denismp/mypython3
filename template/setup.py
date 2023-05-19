9b1c4765695d250e7f1105812352ee2726633ac2
denismp <denisputnam@gmail.com>
2023-05-19 16:46:11 -0400

    Added more instructions to setup.py.

--- template/setup.py
+++ template/setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
     
 setuptools.setup(
-    name="test_suite",
+    name="test-suite",
     version="0.0.1",
     author="Denis",
     description="This is my application",
@@ -32,7 +32,10 @@ The build_package.py must copy ALL the .py, .jar, .yaml, .json, and any data fil
 1. cd to the build directory.
 2. Run python build_package.py - this must be written to create the testsuite_pkg.
 3. cd to the testsuite_pkg directory.
-4. Run python setup.py sdist
+4. Run python setup.py sdist bdist_wheel
 5. cd to the testsuite_pkg/dist directory.
-6. Run pip install test_suite-0.0.1.tar.gz    
+6. Run pip unistall test-suite
+7. commit and push any changes.
+8. Run pip install test_suite-0.0.1-py3-none-any.whl
+9. Run testsuite -h or -l to make sure it installed.
 """
